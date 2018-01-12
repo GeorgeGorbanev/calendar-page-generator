45b0b9e45d34e9f28e9062630341ae63ee2f039d
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2018-01-12 22:27:15 +0300

    CalendarPage: #previousDates, #currentDates, #nextDates, #allDates, #year, #month

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-module.exports = {
-  forSingleDate: function (date) {
-    // generate here
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+const generatePreviousMonthDates = function (year, month) {
+  const firstMonthDay = new Date(year, month, 1, 12);
+  let dates = [];
+  if (firstMonthDay.getDay() !== 1) {
+    let currentDay = firstMonthDay;
+    while (currentDay.getDay() !== 1) {
+      let previousDay = Number(currentDay) - ONE_DAY;
+      previousDay = new Date(previousDay);
+      dates.unshift(previousDay);
+      currentDay = previousDay;
+    }
   }
+  return dates;
 };
+
+const generateCurrentMonthDates = function (year, month) {
+  const firstMonthDay = new Date(year, month, 1, 12);
+  let dates = [firstMonthDay];
+  let nextDay = firstMonthDay;
+  for (;;) {
+    nextDay = Number(nextDay) + ONE_DAY;
+    nextDay = new Date(nextDay);
+    if (nextDay.getMonth() === month) {
+      dates.push(nextDay);
+    } else {
+      break;
+    }
+  }
+  return dates;
+};
+
+const generateNextMonthDates = function (year, month) {
+  let dates = [];
+  let nextDay = new Date(year, month + 1, 1, 12);
+  if (nextDay.getDay() !== 1) {
+    dates.push(nextDay);
+    for (;;) {
+      nextDay = Number(nextDay) + ONE_DAY;
+      nextDay = new Date(nextDay);
+      if (nextDay.getDay() === 1) {
+        break;
+      } else {
+        dates.push(nextDay);
+      }
+    }
+  }
+  return dates;
+};
+
+class CalendarPage {
+  constructor(date) {
+    this.year = date.getFullYear();
+    this.month = date.getMonth();
+    this.previousDates = generatePreviousMonthDates(this.year, this.month);
+    this.currentDates = generateCurrentMonthDates(this.year, this.month);
+    this.nextDates = generateNextMonthDates(this.year, this.month);
+    this.allDates = [].concat(this.previousDates).concat(this.currentDates).concat(this.nextDates);
+  }
+}
+
+module.exports = CalendarPage;
---
+++ test/calendar_page.js
@@ -0,0 +1,105 @@
+const assert = require('assert');
+const CalendarPage = require('../index.js');
+
+describe('CalendarPage', function() {
+  describe('with date 2018-02-01', function() {
+    const date = new Date(2018, 1, 1);
+    const page = new CalendarPage(date);
+
+    it('#year', function() {
+      assert.equal(page.year, 2018);
+    });
+
+    it('#month', function() {
+      assert.equal(page.month, 1);
+    });
+
+    describe('#previousDates', function() {
+      const dates = page.previousDates;
+
+      it('is Array', function() {
+        assert.equal(Array.isArray(dates), true);
+      });
+
+      it('with length 3', function() {
+        assert.equal(dates.length, 3);
+      });
+
+      it('with first element date is 2018-01-29', function() {
+        const firstDateFormated = dates[0].toISOString().slice(0, 10);
+        assert.equal(firstDateFormated, '2018-01-29');
+      });
+
+      it('with last element date is 2018-01-31', function() {
+        const lastDateFormated = dates[dates.length - 1].toISOString().slice(0, 10);
+        assert.equal(lastDateFormated, '2018-01-31');
+      });
+    });
+
+    describe('#currentDates', function() {
+      const dates = page.currentDates;
+
+      it('is Array', function() {
+        assert.equal(Array.isArray(dates), true);
+      });
+
+      it('with length 28', function() {
+        assert.equal(dates.length, 28);
+      });
+
+      it('with first element date is 2018-02-01', function() {
+        const firstDateFormated = dates[0].toISOString().slice(0, 10);
+        assert.equal(firstDateFormated, '2018-02-01');
+      });
+
+      it('with last element date is 2018-02-28', function() {
+        const lastDateFormated = dates[dates.length - 1].toISOString().slice(0, 10);
+        assert.equal(lastDateFormated, '2018-02-28');
+      });
+    });
+
+    describe('#nextDates', function() {
+      const dates = page.nextDates;
+
+      it('is Array', function() {
+        assert.equal(Array.isArray(dates), true);
+      });
+
+      it('with length 4', function() {
+        assert.equal(dates.length, 4);
+      });
+
+      it('with first element date is 2018-03-01', function() {
+        const firstDateFormated = dates[0].toISOString().slice(0, 10);
+        assert.equal(firstDateFormated, '2018-03-01');
+      });
+
+      it('with last element date is 2018-03-04', function() {
+        const lastDateFormated = dates[dates.length - 1].toISOString().slice(0, 10);
+        assert.equal(lastDateFormated, '2018-03-04');
+      });
+    });
+
+    describe('#allDates', function() {
+      const dates = page.allDates;
+
+      it('is Array', function() {
+        assert.equal(Array.isArray(dates), true);
+      });
+
+      it('with length 35', function() {
+        assert.equal(dates.length, 35);
+      });
+
+      it('with first element date is 2018-01-29', function() {
+        const firstDateFormated = dates[0].toISOString().slice(0, 10);
+        assert.equal(firstDateFormated, '2018-01-29');
+      });
+
+      it('with last element date is 2018-03-04', function() {
+        const lastDateFormated = dates[dates.length - 1].toISOString().slice(0, 10);
+        assert.equal(lastDateFormated, '2018-03-04');
+      });
+    });
+  });
+});
